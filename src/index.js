3dce8c7260c34ed3af8296162302331b47181d25
Hariom Bamboriya <dhakadhariom007@gmail.com>
2025-09-06 14:11:23 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -5,18 +5,12 @@ const cookieParser = require("cookie-parser");
 require("dotenv").config();
 
 const allowedOrigins = [
-  "http://localhost:5173",
-  "http://13.60.156.87"
+  "http://13.60.156.87",
+  "http://localhost:5173"
 ];
 
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
+  origin: allowedOrigins,
   credentials: true
 }));
 
